c4bc789198936eaaa4649d12ed659ed1268ac512
applqpak <applqpak@gmail.com>
2016-02-19 21:25:16 +1100

    Update index.php

--- src/web/html/index.php
+++ src/web/html/index.php
@@ -1,1 +1,27 @@
+<!DOCTYPE html>
 
+  <html>
+
+    <head>
+
+      <meta name="apple-mobile-web-app-capable" content="yes">
+
+      <meta name="viewport" content="width=device-width">
+
+      <link rel="apple-touch-icon" href="http://i.imgur.com/DK1qg2a.png">
+
+      <link rel="apple-touch-icon" sizes="60x60" href="http://i.imgur.com/NZbhD1L.png">
+
+      <link rel="apple-touch-icon" sizes="114x114" href="http://i.imgur.com/ifRHYPM.png">
+
+      <link rel="apple-touch-icon" sizes="144x144" href="http://i.imgur.com/7iFj84S.png">
+
+    </head>
+
+    <body>
+
+      <strong><em>PigRaid iOS App Is Still W.I.P! This App Is Developed By @ItzJavaCraft And @applqpak</em></strong>
+
+    </body>
+
+  </html>
